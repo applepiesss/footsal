348bd25111d7e02bbe40510106641ced9da6fdbb
applepiesss <nadiaaiiisyahf@gmail.com>
2025-09-08 02:26:31 +0700

    Tugas 2

---
+++ footsal/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for footsal project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'footsal.settings')
+
+application = get_asgi_application()
